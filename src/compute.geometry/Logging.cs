48a1f242f3ac340196eeaf3c5d94f61308e720ee
Will Pearson <will@mcneel.com>
2020-09-25 16:38:29 +0100

    Properly deprecate logging env vars
    Ugh, it's a mess but someone somewhere might be using these...

--- src/compute.geometry/Logging.cs
+++ src/compute.geometry/Logging.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using Serilog;
 using Serilog.Formatting.Json;
 
@@ -13,10 +14,9 @@ namespace compute.geometry
             if (_enabled)
                 return;
 
-            var default_dir = Path.Combine(Path.GetTempPath(), "Compute", "Logs");
-            var dir = Env.GetEnvironmentString("RHINO_COMPUTE_LOG_PATH", default_dir);
+            var dir = GetLogDir(Path.Combine(Path.GetTempPath(), "Compute", "Logs"));
             var path = Path.Combine(dir, "log-geometry-.txt"); // log-geometry-20180925.txt, etc.
-            var limit = Env.GetEnvironmentInt("RHINO_COMPUTE_LOG_RETAIN_DAYS", 10);
+            var limit = GetLogLimit(10);
 
             var logger = new LoggerConfiguration()
 #if DEBUG
@@ -28,9 +28,44 @@ namespace compute.geometry
                 .WriteTo.File(new JsonFormatter(), path, rollingInterval: RollingInterval.Day, retainedFileCountLimit: limit);
 
             Log.Logger = logger.CreateLogger();
+
+            // log warnings if deprecated env vars used (see helpers below)
+            foreach (var msg in _warnings)
+                Log.Warning(msg);
+
             Log.Debug("Logging to {LogPath}", Path.GetDirectoryName(path));
 
             _enabled = true;
+        }
+
+        // helper methods to handle renamed environment variables without breaking things for anyone
+        // using the existing names
+        // TODO: clean up this mess in a few months time!
+
+        private static List<string> _warnings = new List<string>();
+
+        private static string GetLogDir(string default_dir)
+        {
+            var dir = Env.GetEnvironmentString("RHINO_COMPUTE_LOG_PATH", null);
+            if (dir == null)
+            {
+                dir = Env.GetEnvironmentString("COMPUTE_LOG_PATH", null);
+                if (dir != null)
+                    _warnings.Add($"COMPUTE_LOG_PATH is deprecated; use RHINO_COMPUTE_LOG_PATH instead");
+            }
+            return dir ?? default_dir;
+        }
+
+        private static int GetLogLimit(int default_limit)
+        {
+            var limit = Env.GetEnvironmentInt("RHINO_COMPUTE_LOG_RETAIN_DAYS", 0);
+            if (limit == 0)
+            {
+                limit = Env.GetEnvironmentInt("COMPUTE_LOG_RETAIN_DAYS", 0);
+                if (limit != 0)
+                    _warnings.Add($"COMPUTE_LOG_RETAIN_DAYS is deprecated; use RHINO_COMPUTE_LOG_RETAIN_DAYS instead");
+            }
+            return limit != 0 ? limit : default_limit;
         }
     }
 }
